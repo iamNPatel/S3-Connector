0409836ace49d15350dada1c191a63cf6e38ebe2
iamNPatel <nipatel@cars.com>
2023-04-12 15:36:47 -0400

    Update connector.py

--- connector.py
+++ connector.py
@@ -1,21 +1,13 @@
 # csv conditions
 class Connector:
-    def __init__(self, user, file_type):
+    def __init__(self, user):
         if user == 'ml': 
             self.user = 'ml_usr'
         elif user == 'dw':
             self.user = 'dw_usr'
         else:
             return "please choose a 'dw' or 'ml' user"
-        
-        #avro / zip
-        if file_type == 'parquet':
-            self.type = 'parquet'
-        elif file_type == 'csv':
-            self.type = 'csv'
-        else:
-            return "please choose from 'parquet' or 'csv' file types"
-        
+                
     def __repr__(self):
         return f"S3 connector for {self.user}. Configured for {self.type} files"
     
@@ -45,17 +37,19 @@ class Connector:
         return url
 
     # read a single s3 partition to a df from s3 
-    def read_single_partition_s3(self, bucket, env, dataset, date, protected):
+    def read_single_partition_s3(self, bucket, env, dataset, date, protected, csv=False, avro=False, zip=False):
         url = self.create_s3_url(bucket, env, dataset, date, protected)
-        if self.type == 'parquet':
-            df = spark.read.parquet(url)
-            return df
-        else: 
-            df = spark.read.csv(url)
-            return df 
+        if csv:
+            return spark.read.option("header",True).csv(url)
+        elif avro:
+            return spark.read.format('avro').load(url)
+        elif zip:
+            pass
+        else:
+            return spark.read.parquet(url)
 
     # read a range of date partitions from s3 to a df
-    def read_multiple_partitions_s3(self, bucket, env, dataset, date, _range, cols, protected):
+    def read_multiple_partitions_s3(self, bucket, env, dataset, date, _range, cols, protected, csv=False, avro=False, zip=False):
         urls = []
         start = datetime.strptime(date, "%Y-%m-%d")
         for _ in range(_range):
@@ -63,26 +57,37 @@ class Connector:
             urls.append(url)
             start -= timedelta(days=1)
         path = self.create_s3_basepath(bucket, env, dataset, protected)
-        # Needs to specify parquet or csv
-        if len(cols) > 0:
-            df = spark.read.option('basePath', path).option("mergeSchema", "true").load(urls).select(cols)
-        else:
-            df = spark.read.option('basePath', path).option("mergeSchema", "true").load(urls)
-        return df
+        if csv:
+            return spark.read.csv([urls])
+        elif avro:
+            pass
+        elif zip:
+            pass
+        else:    
+            if len(cols) > 0:
+                return spark.read.option('basePath', path).option("mergeSchema", "true").load(urls).select(cols)
+            else:
+                return spark.read.option('basePath', path).option("mergeSchema", "true").load(urls)
+        
 
     # parent function for reading from s3 - accepts optional arguments for range (multiple dates), cols (specific cols for partitions), and protected (bucket)
     # date = 0 for datasets without (date) partitions
-    def read_from_s3(self, bucket, env, dataset, date=0, _range=1, cols=[], protected=False):
+    def read_from_s3(self, bucket, env, dataset, date=0, _range=1, cols=[], protected=False, csv=False, avro=False, zip=False):
         if range == 1:
-            return self.read_single_partition_s3(bucket, env, dataset, date, protected)
+            return self.read_single_partition_s3(bucket, env, dataset, date, protected, csv=False, avro=False, zip=False)
         else:
-            return self.read_multiple_partitions_s3(bucket, env, dataset, date, _range, cols, protected)
+            return self.read_multiple_partitions_s3(bucket, env, dataset, date, _range, cols, protected, csv=False, avro=False, zip=False)
 
     # write a df to s3 (landing), optional argument for number of partitions 
-    def write_to_s3(self, df, bucket, env, dataset, date=0, partitions=1):
+    def write_to_s3(self, df, bucket, env, dataset, date=0, partitions=1, csv=False, avro=False, zip=False):
         url = self.create_s3_url(bucket, env, dataset, date)
-        if self.type == 'parquet':
-            df.repartition(partitions).write.parquet(url, mode='overwrite')
-        else:
+        if csv:
             df.repartition(partitions).write.mode("overwrite").csv(url)
+        elif avro:
+            df.write.format('avro').mode('overwrite').save(url)
+        elif zip:
+            # write zip file
+            pass
+        else:
+            df.repartition(partitions).write.parquet(url, mode='overwrite')
 
